819af80f17539da6459550d642181316847e7de2
Berkay Ates <atesberkay2356@gmail.com>
2025-05-26 19:26:34 +0300

    Refactor RawImageExtractorCV2 class to improve image loading methods and fix variable usage

--- src/siglip4cc/raw_image_util.py
+++ src/siglip4cc/raw_image_util.py
@@ -1,4 +1,4 @@
-import torch as th
+import torch
 import numpy as np
 from PIL import Image
 
@@ -16,6 +16,17 @@ class RawImageExtractorCV2:
         self.size = size
         self.transform = AutoProcessor.from_pretrained("google/siglip-base-patch16-224")
 
+    def load_image(self, data):
+
+        if isinstance(data, Image.Image):
+            return self.get_image_data(data)
+
+        elif isinstance(data, (str, Path)):
+            return self.get_image_data_path(data)
+
+        elif isinstance(data, torch.Tensor):  # Eğer Tensor ise
+            return self.get_image_data_tensor(data)
+
     def image_to_tensor(self, image_file, preprocess):
         image = Image.open(image_file).convert("RGB")
         image_data = preprocess(images=image, return_tensors="pt")
@@ -23,8 +34,22 @@ class RawImageExtractorCV2:
         return {"image": image_data}
 
     def get_image_data(self, image_path):
+        image_loaded = image_loaded.convert("RGB")
+        image_loaded = self.transform(image_loaded)
+
+        return {"image": image_loaded}
+
+    def get_image_data_path(self, image_path):
         image_input = self.image_to_tensor(image_path, self.transform)
         return image_input
+
+    def get_image_data_tensor(self, image_tensor):
+        numpy_img = (tensor.permute(1, 2, 0).cpu().numpy() * 255).astype(np.uint8)
+
+        image = Image.fromarray(numpy_img)
+
+        # PIL görüntüsüne çevir
+        return self.get_image_data(image)
 
 
 # An ordinary video frame extractor based CV2
